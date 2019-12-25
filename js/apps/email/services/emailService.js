a73992560d0cbb04bb66db9a1d0f8b3116e366cd
Alina <alina.boshkov@gmail.com>
2019-12-25 11:26:51 +0200

    email service

--- js/apps/email/services/emailService.js
+++ js/apps/email/services/emailService.js
@@ -1,7 +1,19 @@
 'use strict'
 import storageService from '.../services/storageService.js'
+export default { getEmails }
 
-let gEmails;
+let gEmails = storageService.load('gEmails') || createEmails()
+
+
+function getEmails(query) {
+    const emails = [...gEmails]
+    // const emails = !query ? [...gEmails]
+    //     : gEmails.filter(email => {
+    //         return (book.title.includes(query.name) &&
+    //             (book.listPrice.amount > query.priceFrom) && (book.listPrice.amount < query.priceTo))
+    //     });
+    return Promise.resolve(emails)
+}
 
 function createEmails(subject, body, isRead, sentAt) {
     return email = {
@@ -13,15 +25,15 @@ function createEmails(subject, body, isRead, sentAt) {
 }
 
 function createEmails() {
-gEmails = []
-gEmails.push({ subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt : 1551133930594 })
-gEmails.push({ subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt : 1551133930594 })
-gEmails.push({ subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt : 1551133930594 })
-gEmails.push({ subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt : 1551133930594 })
-gEmails.push({ subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt : 1551133930594 })
-gEmails.push({ subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt : 1551133930594 })
-gEmails.push({ subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt : 1551133930594 })
+    gEmails = []
+    gEmails.push({ subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt: 1551133930594 })
+    gEmails.push({ subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt: 1551133930594 })
+    gEmails.push({ subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt: 1551133930594 })
+    gEmails.push({ subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt: 1551133930594 })
+    gEmails.push({ subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt: 1551133930594 })
+    gEmails.push({ subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt: 1551133930594 })
+    gEmails.push({ subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt: 1551133930594 })
 
-storageService.store('gEmails', gEmails)
-return gEmails
+    storageService.store('gEmails', gEmails)
+    return gEmails
 }
