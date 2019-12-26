9b3428662626a11ed38dd6c1924c3adf4b260eb2
Alina <alina.boshkov@gmail.com>
2019-12-26 17:50:40 +0200

    fixed emails layout a little bit

--- js/apps/email/emailCmp/EmailPreview.jsx
+++ js/apps/email/emailCmp/EmailPreview.jsx
@@ -4,7 +4,7 @@ export default class EmailPreview extends React.Component {
     render() {
         const email = this.props.email;
         return <Link to={`/email/${email.id}`}>
-            <li className={(email.isRead) ? 'read' : 'not-read'}>{email.subject}{email.body}{email.sentAt}</li>
+            <li className={(email.isRead) ? 'read' : 'not-read'}><div>{email.subject}</div> {email.body}{email.sentAt}</li>
         </Link>
     }
 }
