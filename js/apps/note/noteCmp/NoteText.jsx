13511748179b514642b0568be0ed772fae44e4e2
Yoad Gantz <yoad08@gmail.com>
2019-12-26 15:49:55 +0200

    less conlogs

--- js/apps/note/noteCmp/NoteText.jsx
+++ js/apps/note/noteCmp/NoteText.jsx
@@ -3,8 +3,6 @@ import NoteEditControl from "./NoteEditControl.jsx";
 export default class NoteText extends React.Component {
 
     render() {
-        console.log('?');
-
         let style = this.props.note.style || 'none';
         let bgc = style.backgroundColor || 'rgba(0,0,0,0)';
         let color = style.color || 'black';
@@ -17,6 +15,5 @@ export default class NoteText extends React.Component {
                 onChangeColor={this.props.onChangeColor}
             ></NoteEditControl>
         </li>
-
     }
 }
