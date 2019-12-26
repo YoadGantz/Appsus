cc0e88b2eabf5321c0bb7510adc4c719d5924940
Yoad Gantz <yoad08@gmail.com>
2019-12-27 00:14:17 +0200

    fix another edit style bug

--- js/apps/note/noteCmp/NoteEditControl.jsx
+++ js/apps/note/noteCmp/NoteEditControl.jsx
@@ -3,7 +3,7 @@ const { Link } = ReactRouterDOM
 export default class NoteEditControl extends React.Component {
 
     render() {
-        console.log(this.props.note.info.txtInput)
+        const note = this.props.note
         return <React.Fragment>
             <label>bgc
             <input onChange={(ev) => {
@@ -13,8 +13,8 @@ export default class NoteEditControl extends React.Component {
             <label>color
                 <input onChange={(ev) => this.props.onChangeColor(note, ev.target.value)} type="color" name="font-color" id="font-color" />
             </label>
-            <button onClick={() => this.props.delete(this.props.note)}>X</button>
-            <Link to={`/note/edit/${this.props.note.id}`}><img height="20px"src="../../../../imgs/icons/edit.svg" /></Link>
+            <button onClick={() => this.props.delete(note)}>X</button>
+            <Link to={`/note/edit/${note.id}`}><img height="20px"src="../../../../imgs/icons/edit.svg" /></Link>
         </React.Fragment>
     }
 }
