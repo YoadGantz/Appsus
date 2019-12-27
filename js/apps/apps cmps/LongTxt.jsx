00941f3ab8248d4c610b465b44011dc4242e7cfc
Alina <alina.boshkov@gmail.com>
2019-12-28 01:02:52 +0200

    implimented long text for notes

--- js/apps/apps cmps/LongTxt.jsx
+++ js/apps/apps cmps/LongTxt.jsx
@@ -15,14 +15,14 @@ export default class LongTxt extends React.Component {
         this.setState(prevstate => ({ isLongTxtShown: !prevstate.isLongTxtShown }))
     }
 
-    getButtonTxt(){
-        return this.state.isLongTxtShown && <img onClick = {this.props.onExpand} height="20px" src="../../imgs/icons/expand.svg"/>
+    getExpandButton(){
+        return <img onClick = {this.props.onExpand} height="20px" src="../../imgs/icons/expand.svg"/>
     }
 
     render() {
         return (
             <span className="read-more" onClick={this.handleReadMore}>{this.getText()}
-            { this.props.text.length >= this.props.shortLength && <span className="read-more-button">{this.getButtonTxt()}</span>}
+            { this.props.text.length >= this.props.shortLength && <span className="read-more-button">{this.getExpandButton()}</span>}
             </span>
         )
     }
--- js/apps/note/noteCmp/NoteText.jsx
+++ js/apps/note/noteCmp/NoteText.jsx
@@ -1,4 +1,5 @@
 import NoteEditControl from "./NoteEditControl.jsx";
+import LongTxt from "../../apps cmps/LongTxt.jsx"
 
 export default class NoteText extends React.Component {
     onCreateEmail = () => {
@@ -6,20 +7,25 @@ export default class NoteText extends React.Component {
         this.props.onCreateEmail(emailBody)
     }
 
+    onExpand = () => {
+        this.props.goToDetails(this.props.note.id)
+    }
+
     render() {
         let style = this.props.note.style || 'none';
         let bgc = style.backgroundColor || 'rgba(0,0,0,0)';
         let color = style.color || 'black';
         return <li style={{ backgroundColor: bgc, color: color }}>
-            {this.props.note.info.txtInput}
-            <NoteEditControl
-                togglePin={this.props.togglePin}
-                delete={this.props.delete}
-                note={this.props.note}
-                onChangeBGColor={this.props.onChangeBGColor}
-                onChangeColor={this.props.onChangeColor}
-            ></NoteEditControl>
-            <img title="create an Email"onClick={this.onCreateEmail} height="25px" src="../imgs/icons/email.svg" />
-        </li>
+                    <LongTxt onExpand={this.onExpand} text={this.props.note.info.txtInput} shortLength={30}>
+                    </LongTxt>
+                    <NoteEditControl
+                        togglePin={this.props.togglePin}
+                        delete={this.props.delete}
+                        note={this.props.note}
+                        onChangeBGColor={this.props.onChangeBGColor}
+                        onChangeColor={this.props.onChangeColor}>
+                    </NoteEditControl>
+                    <img title="create an Email" onClick={this.onCreateEmail} height="25px" src="../imgs/icons/email.svg" />
+               </li>
     }
 }
