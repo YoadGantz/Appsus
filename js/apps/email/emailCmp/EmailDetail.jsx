e57923ff4491dfe7aec28ad7157c05e3c28ab026
Yoad Gantz <yoad08@gmail.com>
2019-12-28 12:40:25 +0200

    updated sorting and timestamp

--- js/apps/email/emailCmp/EmailDetail.jsx
+++ js/apps/email/emailCmp/EmailDetail.jsx
@@ -1,15 +1,25 @@
 export default class EmailDetail extends React.Component {
     // create utils to show date and time nice format 
     //update to show the time if its today or date if not 
+
+    showSentAtFormatted = (email) => {
+        let date = new Date(email.sentAt)
+        if (date.getDate() === new Date().getDate()) return `${date.getHours()}:${date.getMinutes()}`
+        return `${date.getDate()}/${date.getMonth()+1}`
+    }
+
+
     render() {
+        const email = this.props.email;
+        const sentAt = this.showSentAtFormatted(email)
         return <div>
             <span onClick={this.props.goBack}>BACK</span>
             <img onClick={this.props.onCreateNote} title = "Save to notes" height="20px" src="../../../imgs/icons/send.svg"/>
             <img onClick={this.props.delete} height="20px" src="../imgs/icons/delete.svg"/>
             <div>
-                <p>{this.props.email.sentAt}</p>
-                <h3><span>Subject</span>: {this.props.email.subject}</h3>
-                <p>{this.props.email.body}</p>
+                <p>{sentAt}</p>
+                <h3><span>Subject</span>: {email.subject}</h3>
+                <p>{email.body}</p>
             </div>
         </div>
     }
