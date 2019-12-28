1de02aafb31e4f0b0db33a9f4c682bc1ce990cc3
Alina <alina.boshkov@gmail.com>
2019-12-28 10:52:11 +0200

    improved read more less buttons

--- js/apps/email/emailCmp/EmailPreview.jsx
+++ js/apps/email/emailCmp/EmailPreview.jsx
@@ -19,9 +19,9 @@ export default class EmailPreview extends React.Component {
                     <input type="checkbox" onClick={this.onSelect} />
                     <img className={(email.isStarred) ? 'star' : 'un-star'} height="10px" src="../../../../imgs/icons/star.svg" onClick={this.onStar} />
                     <span>
-                        {email.subject}
-                        <LongTxt onExpand={this.onExpand} text={email.body} shortLength={80}>
+                        <LongTxt onExpand={this.onExpand} text={email.subject + email.body} shortLength={80}>
                         </LongTxt>
+                        <img onClick={this.onExpand} height="20px" src="../../imgs/icons/expand.svg" />
                         {email.sentAt}
                     </span>
         </li>
--- js/apps/note/noteCmp/NoteImage.jsx
+++ js/apps/note/noteCmp/NoteImage.jsx
@@ -5,13 +5,17 @@ export default function NoteImage(props) {
     let bgc = style.backgroundColor || 'rgba(0,0,0,0)';
     let color = style.color || 'black';
     return <React.Fragment>
-        <li style={{ backgroundColor: bgc, color: color }}><img src={props.note.info.txtInput} /></li>
-        <NoteEditControl
-            togglePin={props.togglePin}
-            delete={props.delete}
-            note={props.note}
-            onChangeBGColor={props.onChangeBGColor}
-            onChangeColor={props.onChangeColor}
-        ></NoteEditControl>
+        <li class-name="image-note" style={{ backgroundColor: bgc, color: color }}><img src={props.note.info.txtInput} />
+            <span className="note-controls">
+
+                <NoteEditControl
+                    togglePin={props.togglePin}
+                    delete={props.delete}
+                    note={props.note}
+                    onChangeBGColor={props.onChangeBGColor}
+                    onChangeColor={props.onChangeColor}>
+                </NoteEditControl>
+            </span>
+        </li>
     </React.Fragment>
 }
